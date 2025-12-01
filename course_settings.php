4f4bcd8b2a12629df9e33c785069cdfd9ffd9489
Cucorn <zeromassmedia@gmail.com>
2025-12-01 14:36:34 -0500

    Update course_settings.php

--- course_settings.php
+++ course_settings.php
@@ -24,6 +24,7 @@
 
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
+require_once(__DIR__ . '/lib.php');
 
 $courseid = required_param('id', PARAM_INT);
 
