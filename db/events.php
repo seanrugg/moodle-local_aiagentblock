2d38008c6f96dd0caf367b71e446fd8b4d522862
Cucorn <zeromassmedia@gmail.com>
2025-12-05 08:02:56 -0500

    Update events.php

--- db/events.php
+++ db/events.php
@@ -1,7 +1,35 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Event observer definitions for AI Agent Blocker plugin
+ *
+ * @package    local_aiagentblock
+ * @copyright  2025
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
 $observers = [
     [
         'eventname' => '\mod_quiz\event\attempt_submitted',
-        'callback' => '\local_aiagentblock\observer::quiz_attempt_finished',
+        'callback' => '\local_aiagentblock\observer::quiz_attempt_submitted',
+        'includefile' => null,
+        'priority' => 0,
+        'internal' => true,
     ],
 ];
