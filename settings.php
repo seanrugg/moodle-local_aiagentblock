e7bedc9bbf240d3e3fc0b01ff5382da65ca9dbe7
Cucorn <zeromassmedia@gmail.com>
2025-12-12 22:12:50 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,38 +1,15 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Site administration settings for AI Agent Blocker plugin
- *
- * @package    local_aiagentblock
- * @copyright  2024
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 
 defined('MOODLE_INTERNAL') || die();
 
 if ($hassiteconfig) {
     
-    // Create settings page
     $settings = new admin_settingpage(
         'local_aiagentblock',
         get_string('pluginname', 'local_aiagentblock')
     );
     
-    // Add settings page to local plugins category
     $ADMIN->add('localplugins', $settings);
     
     // ========== GENERAL SETTINGS ==========
@@ -48,6 +25,14 @@ if ($hassiteconfig) {
         get_string('enabled', 'local_aiagentblock'),
         get_string('enabled_desc', 'local_aiagentblock'),
         1
+    ));
+    
+    // ========== DATA COLLECTION MODE ========== NEW!
+    $settings->add(new admin_setting_configcheckbox(
+        'local_aiagentblock/analysis_mode',
+        get_string('analysis_mode', 'local_aiagentblock'),
+        get_string('analysis_mode_desc', 'local_aiagentblock'),
+        0
     ));
     
     // Block access immediately
